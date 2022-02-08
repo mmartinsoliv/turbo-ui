ef21c1b6be801918db473e2ea170dbff4e1bb19d
matheus martins <mathews_2010@outlook.com>
2022-02-08 20:15:30 -0300

    initial commit

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,9 +1,10 @@
-import { Button } from "ui";
+import { Button, Input } from "ui";
 
 export default function Web() {
   return (
     <div>
       <h1>Web</h1>
+      <Input placeholder="name" />
       <Button />
     </div>
   );
---
+++ packages/ui/Input.tsx
@@ -0,0 +1,4 @@
+
+export const Input = (props: React.InputHTMLAttributes<HTMLInputElement>) => (
+    <input {...props} />
+)
