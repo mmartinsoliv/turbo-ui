e538e4834fc5981ea65f4c06178a006b7a4a09a6
matheus martins <mathews_2010@outlook.com>
2022-02-08 21:42:49 -0300

    fix: ci

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,4 +1,4 @@
-const withTM = require("next-transpile-modules")(["ui"]);
+const withTM = require("next-transpile-modules")(["@turbo-ui/core"]);
 
 module.exports = withTM({
   reactStrictMode: true,
